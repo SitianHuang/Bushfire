c148df250ea6f7503bbd9029c940fedbc482af99
SitianHuang <90133112+SitianHuang@users.noreply.github.com>
2021-10-18 03:20:48 +0800

    Add files via upload

---
+++ vega_lite_vis3.js
@@ -0,0 +1,4 @@
+var vg_1 = "index3.json";
+vegaEmbed("#barPlot", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
